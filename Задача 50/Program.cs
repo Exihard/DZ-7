aa6caec148ada43b45b329b3ddae7e4ce3de8065
Korehov_Roman_Nikolaevich <rkorekhov@yandex.ru>
2022-12-14 00:29:12 +0300

    DZ-7

---
+++ Задача 50/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 50: Написать программу, которая на вход принимает позиции элемнта в двумерном массиве, и возвращает значение этого элемента
+// или же указание, что такого элемента нет.
+/* Пример: задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет! */
+
+Console.WriteLine("Введите количество строк");
+int line = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int column = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите номер строки");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите номер столбца");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int [,] num = new int [line,column];
+FillArrayRandomNumbers(num);
+
+if (n > num.GetLength(0) || m > num.GetLength(1))
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Такого элемента нет!");
+    Console.ResetColor();
+}
+else
+{
+    Console.Write($"Значение {n}-го элемента строки и {m}-го столбца равно: ");
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"{num[n-1,m-1]}");
+    Console.ResetColor();
+}
+
+PrintArray(num);
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {        
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array [i,j] = new Random().Next(-100, 100)/10;
+            }   
+        }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }   
+        Console.Write("");
+        Console.WriteLine(""); 
+    }
+}
---
+++ Задача 52/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 52: Задать двумерный массив из целых чисел. Найти средне арифметическое элементов в каждом столбце.
+/* Например: задан массив:
+1 4 7 2
+5 9 3 2
+8 4 2 4
+Среднее арифметическое каждого столбца 4,6; 5,6; 3,6; 3. */
+Console.WriteLine("введите количество строк");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int[,] num = new int[n, m];
+FillArrayRandomNum(num);
+
+
+for (int j = 0; j < num.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < num.GetLength(0); i++)
+    {
+        avarage = (avarage + num[i, j]);
+    }
+    avarage = avarage / n;
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write(avarage + "; ");
+    Console.ResetColor();
+    
+}
+Console.WriteLine();
+PrintArray(num);
+
+
+
+void FillArrayRandomNum(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("");
+        Console.WriteLine("");
+    }
+}   
